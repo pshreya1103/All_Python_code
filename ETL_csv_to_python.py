da2aa69cc0bea3a8497ea0b87353d596f09017ba
Shreya Pandey <shreyapandey@Rahuls-MacBook-Pro.local>
2024-03-28 09:49:41 +0530

    All fn definition added

--- ETL_csv_to_python.py
+++ ETL_csv_to_python.py
@@ -2,17 +2,32 @@ import pandas as pd
 import numpy as np
 from datetime import datetime as dt
 
-data_ip = pd.read_csv('car_prices.csv')
-data_ip_mod=data_ip.dropna()
+INPUT_FILE_PATH = '/Users/shreyapandey/Desktop/Python-practice/car_prices.csv'
+OUTPUT_FILE_PATH = '/Users/shreyapandey/Desktop/ETL/car_prices_JSON.json'
 
-my_list = [ ]
-for value in data_ip_mod['saledate']:
-    original_string = value
-    pattern = "GMT"
-    parts = original_string.split(pattern, 1)
-    # data_ip_mod['saledate_only'] = my
-    my_list.append(parts[0])
-data_ip_mod['saledate_only'] = my_list
-data_ip_mod['saledate_only']
+def readetl():
+	return (pd.read_csv(f'{INPUT_FILE_PATH}'))
 
-data_ip_mod.to_json('car_list-json.json', orient='records')
+def transform(df_ip):
+    my_list = [ ]
+    df_ip_mod=df_ip.dropna()
+    for value in df_ip_mod['saledate']:
+        pattern = "GMT"
+        parts_final=extrcatetl(value,pattern)
+        my_list.append(parts_final)
+    df_ip_mod['saledate_dt_only'] = my_list
+    return df_ip_mod
+
+def extrcatetl(string,pattern):
+	parts = string.split(pattern, 1)
+	return(parts[0])
+
+def loadetl(df):
+	df.to_json(f'{OUTPUT_FILE_PATH}', orient='records')
+    # print(f'File loaded at path: {OUTPUT_FILE_PATH}')
+    
+# EXTRACT 
+
+df_ip = readetl() #Extracting data 
+df_op_mod = transform(df_ip)
+loadetl(df_op_mod)
